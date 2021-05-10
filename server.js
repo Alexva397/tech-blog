04f8cea3553ff223ce7bc0651a5a1bbc3280ea9f
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-05-10 17:43:15 -0500

    created package.json, included all dependencies, added static file path and set handlebars as view engine in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SESS_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -20,8 +22,14 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
